5e583fa6bcee3d9acf9e0af93266a922f563dd3f
Hamidreza Baniasadi <hamidreza.neo@gmail.com>
2020-12-05 17:08:48 +0330

    .

---
+++ Keyboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control
+{
+    public class Keyboard
+    {
+        public async Task<object> press(dynamic input)
+        {
+            SendKeys.Send("{ENTER}");
+            return "dfg";
+        }
+    }
+}
